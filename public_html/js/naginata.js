d83f86cb4ced81f33d97a59e38f6191194cf856c
Juga Paazmaya <olavic@gmail.com>
2011-12-19 15:58:51 +0200

    Now also initial has is set.

--- public_html/js/naginata.js
+++ public_html/js/naginata.js
@@ -8,25 +8,29 @@ $(document).ready(function() {
 	if (hash != '') {
 		// Check against available sections.
 		if ($('section' + hash).length == 1) {
-			var title = $('nav a[href=' + hash + ']').attr('title');
-			displaySection(hash, title, 0);
+			displaySection(hash, 0);
 		}
+	}
+	else {
+		displaySection(currentSection, 0);
 	}
 	$('nav a').click(function() {
 		var href = $(this).attr('href');
 		var title = $(this).attr('title');
 		console.log('navigation click. href: ' + href);
-		displaySection(href, title);
+		displaySection(href);
 		return false;
 	});
 });
 
-function displaySection(hash, title, speed) {
+function displaySection(hash, speed) {
 	if (typeof speed === 'undefined') {
 		speed = 200;
 	}
 	$('section:visible').slideUp(speed);
 	$(hash).slideDown(speed);
+	var title = $('nav a[href=' + hash + ']').attr('title');
 	document.title = title + ' - ' + titleSuffix;
 	window.location.hash = hash;
+	currentSection = hash;
 }
