450be9a3845696972af32581ed0710e4fe04a7f1
Juga Paazmaya <olavic@gmail.com>
2011-12-23 20:15:53 +0200

    Editor should be shown in modal colorbox. Not implemented yet.

---
+++ public_html/editor.php
@@ -0,0 +1,25 @@
+<?php
+// Opens in a colorbox (modal: true) with current page as the only get parameter
+
+
+// editor
+
+// make backup and send it to jukka@naginata.fi
+
+// credentials if not logged in, but use only 3rd party auth based on email address.
+// data->users->administrators
+
+/*
+select changes the content of textarea.
+In case the existing does not match the replacement, ask user which one to use.
+*/
+?>
+
+<form action="" method="post">
+	<select name="">
+		<option value=""></option>
+	</select>
+	<textarea name=""></textarea>
+	<input type="submit" value="" />
+</form>
+
