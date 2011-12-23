326a60606032cb281fc9195eafdecb1c7195ad59
Juga Paazmaya <olavic@gmail.com>
2011-12-23 08:11:14 +0200

    Navigation building function. Should make a class out of all this....

--- public_html/index.php
+++ public_html/index.php
@@ -1,5 +1,25 @@
 <?php
-
+$appJson = file_get_contents('../naginata-data.json');
+$appData = json_decode($appJson, true);
+$lang = 'fi';
 
 //etag = sha1( naginata.fi + file/page name + last modification time of the given page )
+
+echo createNavigation($appData['navigation'][$lang]);
+
+
+/**
+ * Navigation data
+ */
+function createNavigation($data) 
+{
+	$out = '<nav><ul>';
+	foreach ($data as $item) 
+	{
+		$out .= '<li><a href="' . $item['0'] . '" title="' . $item['1'] . '">' . $item['2'] . '</a></li>';
+	}
+	$out .= '</ul></nav>';
+	
+	return $out;
+}
 
