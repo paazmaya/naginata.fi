3f56fe98f7bed1751aac82b3257a4bd0d96f4cb9
Juga Paazmaya <olavic@gmail.com>
2012-01-02 23:22:55 +0200

    Configuration shall be in JSON file instead of the traditional PHP.

--- public_html/index.php
+++ public_html/index.php
@@ -2,28 +2,21 @@
 //etag = sha1( naginata.fi + file/page name + last modification time of the given page )
 // In case anyone would edit the json, take a backup first which is then supposed to be committed later.
 
-// Remove www from the url and redirect.
-if (substr($_SERVER['HTTP_HOST'], 0, 3) == 'www')
-{
-	$go = 'http://' . substr($_SERVER['HTTP_HOST'], 4) . $_SERVER['REQUEST_URI'];
-	header('HTTP/1.1 301 Moved Permanently');
-	header('Location: ' . $go);
-	exit();
-}
-session_name('SOFI');
-session_start();
+
+
+require '../libs/ShikakeOji.php';
+
+$shio = new ShikakeOji(realpath('../naginata-data.json'));
+$shio->removeWwwRedirect();
+
 if (!isset($_SESSION['fontcounter']))
 {
 	$_SESSION['fontcounter'] = 0;
 }
 
-require '../libs/ShikakeOji.php';
-require './config.php';
-
-$shio = new ShikakeOji(realpath('../naginata-data.json'));
 $shio->useMinification = false;
 $shio->useTidy = true;
-$shio->emailConfig = $config['email'];
+$shio->loadConfig(realpath('../naginata-data.json'));
 
 $shio->checkRequestedLanguage();
 $shio->checkRequestedPage();
