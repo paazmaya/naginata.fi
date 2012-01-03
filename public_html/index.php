526407026509f68e52cfb6f014fafd326163584e
Juga Paazmaya <olavic@gmail.com>
2012-01-03 18:55:06 +0200

    Why did I even bother, missing Scandinavian characters, thus removing font Luxirri

--- public_html/index.php
+++ public_html/index.php
@@ -3,7 +3,6 @@
 // In case anyone would edit the json, take a backup first which is then supposed to be committed later.
 
 
-
 require '../libs/ShikakeOji.php';
 
 $shio = new ShikakeOji(realpath('../naginata-data.json'));
@@ -16,7 +15,7 @@ if (!isset($_SESSION['fontcounter']))
 
 $shio->useMinification = false;
 $shio->useTidy = true;
-$shio->loadConfig(realpath('../naginata-data.json'));
+$shio->loadConfig(realpath('../naginata-config.json'));
 
 $shio->checkRequestedLanguage();
 $shio->checkRequestedPage();
