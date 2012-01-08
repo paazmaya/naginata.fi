107b0872735aaa4aebc082d9ad1187c2a5ea744b
Juga Paazmaya <olavic@gmail.com>
2012-01-08 20:17:02 +0200

    ini_set in use instead of .htaccess, due to the hosting provider limitations.

--- public_html/index.php
+++ public_html/index.php
@@ -2,8 +2,28 @@
 //etag = sha1( naginata.fi + file/page name + last modification time of the given page )
 // In case anyone would edit the json, take a backup first which is then supposed to be committed later.
 
-ini_set('error_log', realpath('../naginata-php.log'));
+
+
+ini_set('error_log', '../naginata-php.log');
+ini_set('log_errors', 1);
+ini_set('html_errors', 0);
+
+ini_set('date.timezone', 'Europe/Helsinki');
+ini_set('date.default_latitude', '60.174306261926034');
+ini_set('date.default_longitude', '24.983339309692383');
+
+ini_set('arg_separator.output', '&amp;');
+
+ini_set('mbstring.internal_encoding', 'UTF-8');
+mb_internal_encoding('UTF-8');
+
 ini_set('session.referer_check', $_SERVER['HTTP_HOST']);
+ini_set('session.gc_maxlifetime', 86400); // default 1440 seconds = 24 min, 86400 sec = 24 h
+ini_set('session.cache_expire', 1440); // default 180 minutes = 3 h, 1440 min = 24 h
+ini_set('session.cookie_lifetime', 86400);
+ini_set('session.hash_function', 1); // default 0 = md5, 1 = sha1
+
+
 
 require '../libs/ShikakeOji.php';
 
