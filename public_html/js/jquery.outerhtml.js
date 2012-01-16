d0bcf7cb28b4f81be6fbf7599bdfc90f4c73cd12
Juga Paazmaya <olavic@gmail.com>
2012-01-17 00:31:48 +0200

    https://github.com/paazmaya/renshuu.paazmaya.com/blob/master/public_html/js/renshuusurutoki.js

---
+++ public_html/js/jquery.outerhtml.js
@@ -0,0 +1,17 @@
+/**
+ * jQuery.outerHtml
+ * Get the inner and outer html data,
+ * that is the selected element itself including.
+ */
+(function ($) {
+	$.fn.outerHtml = function () {
+		var outer = null;
+		if (this.size()) {
+			var div = $('<div style="display:none"></div>');
+			var clone = $(this[0].cloneNode(false)).html(this.html()).appendTo(div);
+			outer = div.html();
+			div.remove();
+		}
+		return outer;
+	};
+})(jQuery);
