e50c8ce6c26714671b78aba2b4a7654deb62a2b1
Juga Paazmaya <paazmaya@yahoo.com>
2013-10-14 18:44:34 +0300

    Templating via jade so that I will need to learn that too

--- web.js
+++ web.js
@@ -10,44 +10,19 @@ app.use(express.logger());
 app.use(express.static(__dirname + '/public_html'));
 
 
+app.engine('jade', require('jade').__express);
 
-// https://github.com/visionmedia/haml.js/
-// app.engine('.haml', require('hamljs').renderFile);
-
-
-// register .md as an engine in express view system
-app.engine('md', function(path, options, fn){
-  fs.readFile(path, 'utf8', function(err, str){
-    if (err) return fn(err);
-    try {
-      var html = md(str);
-      html = html.replace(/\{([^}]+)\}/g, function(_, name){
-        return options[name] || '';
-      })
-      fn(null, html);
-    } catch(err) {
-      fn(err);
-    }
-  });
-});
 
 app.set('views', __dirname);
+app.set('view engine', 'jade');
 
-// make it the default so we dont need .md
-app.set('view engine', 'md');
-
-
-app.get('/', function(req, res){
-  res.render('README', { title: 'Markdown Example' });
-})
 
 app.get('/:lang', function(req, res){
-  res.render('grading-rules-' + req.params.lang);
-})
-
-app.get('/fail', function(req, res){
-  res.render('missing', { title: 'Markdown Example' });
-})
+  var file = 'grading-rules-' + (req.params.lang || 'engligh') + '.md'
+  var data = fs.readFileSync(file, { encoding: 'utf8' });
+  var html = md(data);
+  res.render('index', { content: html });
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
