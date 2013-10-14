66ee2decc6c28d04fbc7a6a198262425e1dcff13
Juga Paazmaya <paazmaya@yahoo.com>
2013-10-14 17:58:22 +0300

    Getting interesting with Express.js

--- web.js
+++ web.js
@@ -1,10 +1,53 @@
+/** naginata.fi **/
+
+// http://expressjs.com
 var express = require('express');
+var fs = require('fs');
+var md = require('marked').parse;
+
 var app = express();
 app.use(express.logger());
+app.use(express.static(__dirname + '/public_html'));
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+
+
+// https://github.com/visionmedia/haml.js/
+// app.engine('.haml', require('hamljs').renderFile);
+
+
+// register .md as an engine in express view system
+app.engine('md', function(path, options, fn){
+  fs.readFile(path, 'utf8', function(err, str){
+    if (err) return fn(err);
+    try {
+      var html = md(str);
+      html = html.replace(/\{([^}]+)\}/g, function(_, name){
+        return options[name] || '';
+      })
+      fn(null, html);
+    } catch(err) {
+      fn(err);
+    }
+  });
 });
+
+app.set('views', __dirname);
+
+// make it the default so we dont need .md
+app.set('view engine', 'md');
+
+
+app.get('/', function(req, res){
+  res.render('README', { title: 'Markdown Example' });
+})
+
+app.get('/:lang', function(req, res){
+  res.render('grading-rules-' + req.params.lang);
+})
+
+app.get('/fail', function(req, res){
+  res.render('missing', { title: 'Markdown Example' });
+})
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
