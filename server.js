e80129a8a2437e1ae5894f2371955e5fc5836fa7
Juga Paazmaya <olavic@gmail.com>
2013-10-24 08:33:06 +0300

    Does Heroku require empty address?

--- server.js
+++ server.js
@@ -138,7 +138,7 @@ app.get('*', function(req, res) {
 });
 
 // https://devcenter.heroku.com/articles/config-vars
-var ipaddr = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var ipaddr = process.env.OPENSHIFT_NODEJS_IP || null;
 var port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 5000;
 
 app.listen(port, ipaddr, function() {
