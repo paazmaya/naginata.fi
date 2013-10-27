d0178fd96f1360ff8c065357b573234950e97bbb
Juga Paazmaya <olavic@gmail.com>
2013-10-27 03:30:59 +0300

    Initial setup for #3

---
+++ tests/js/sendanmaki_spec.js
@@ -0,0 +1,10 @@
+/** naginata.fi **/
+
+'use strict';
+
+describe('Naginata is in Finland', function() {
+  it('should be known by everyone', function() {
+    expect(true).toBe(true);
+  });
+});
+
