eb158a2c2eb1565de5594e7917a675f97002097d
Juga Paazmaya <olavic@gmail.com>
2013-11-01 00:35:03 +0200

    Passing JSHint #3

--- karma.conf.js
+++ karma.conf.js
@@ -1,11 +1,17 @@
+'use strict';
+
 module.exports = function(config) {
   config.set({
     files: [
+      'bower_components/jquery/jquery.js',
+      'bower_components/colorbox/jquery.colorbox.js',
       'public_html/js/sendanmaki.js',
       'tests/js/*.js'
     ],
     basePath: './',
     browsers: [
+      //'Chrome',
+      //'Firefox',
       'PhantomJS'
     ],
     frameworks: [
@@ -25,6 +31,7 @@ module.exports = function(config) {
     coverageReporter: {
       type: 'lcov',
       dir: 'coverage/'
-    }
+    },
+    singleRun: true
   });
 };
