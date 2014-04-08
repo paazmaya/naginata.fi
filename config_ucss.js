ad099ce818c45e4a8de5c67637949d438c1fceab
Juga Paazmaya <paazmaya@yahoo.com>
2014-04-08 15:50:42 +0300

    uCSS for checking unused CSS, https://github.com/operasoftware/ucss

---
+++ config_ucss.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "pages": {
+    "crawl": "http://localhost:5000/fi"
+  },
+  "headers": {
+    "Accept-Language": "ja-JP"
+  },
+  "css": [
+    "public_html/css/naginata.min.css"
+  ]
+};
