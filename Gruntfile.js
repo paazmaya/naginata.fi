2ad4952512cc8a90b80e1812923e33e80ceceaec
Juga Paazmaya <olavic@gmail.com>
2014-04-17 00:17:22 +0300

    Added time measuring for Grunt task loading, https://github.com/sindresorhus/time-grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,10 +5,10 @@
  * License: Attribution-ShareAlike 3.0 Unported
  *          http://creativecommons.org/licenses/by-sa/3.0/
  */
-
 'use strict';
 
 module.exports = function gruntFile(grunt) {
+  require('time-grunt')(grunt); // Must be first item
 
   // Project configuration.
   grunt.initConfig({
