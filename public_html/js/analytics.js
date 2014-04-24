9911895a016a3f606888dc3ae7a1d1a5e218845d
Juga Paazmaya <olavic@gmail.com>
2014-04-24 07:27:20 +0300

    Whats up with the commas?

--- public_html/js/analytics.js
+++ public_html/js/analytics.js
@@ -12,9 +12,10 @@
   i['GoogleAnalyticsObject'] = r;
   i[r] = i[r] || function ir() {
     (i[r].q = i[r].q || []).push(arguments);
-  }, i[r].l = 1 * new Date();
-  a = s.createElement(o),
-    m = s.getElementsByTagName(o)[0];
+  };
+  i[r].l = 1 * new Date();
+  a = s.createElement(o);
+  m = s.getElementsByTagName(o)[0];
   a.async = 1;
   a.src = g;
   m.parentNode.insertBefore(a, m);
