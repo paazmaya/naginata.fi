236cabaa60be39c4b8d9a11b04d8946d7699a5c1
Juga Paazmaya <olavic@gmail.com>
2014-09-17 01:11:22 +0300

    All them favicons #47

--- source/favicon-creator.js
+++ source/favicon-creator.js
@@ -8,16 +8,21 @@
 
 'use strict';
 
+var fs = require('fs');
+var childProcess = require('child_process');
+
 /**
  * https://github.com/haydenbleasel/favicons
  * npm install favicons
  */
 var favicons = require('favicons');
 
+var startTime = (new Date()).getTime();
+
 favicons({
     // I/O
     source: 'naginata-icon.svg',
-    dest: 'icons',
+    dest: '../public_html/icons',
 
     // Icon Types
     android: true,
@@ -28,13 +33,28 @@ favicons({
     windows: true,
 
     // Miscellaneous
-    html: null,
-    background: '#121212',
+    html: 'favicons.html',
+    background: '#072434',
     tileBlackWhite: false,
     manifest: false,
     trueColor: true,
     logging: true,
     callback: function () {
-      console.log('Something done...');
+      var seconds = Math.round(((new Date()).getTime() - startTime) / 100) / 10;
+      console.log('Completed in ' + seconds + ' seconds. Thanks!');
+
+      // Minify
+      var child = childProcess.execFile(
+        'optipng',
+        ['-o7', '../public_html/icons/*.png'],
+        {cwd: '.'},
+        function (error, stdout, stderr) {
+          console.log(stdout);
+          console.log(stderr);
+        }
+      );
+
+      // Move ico file
+      fs.renameSync('../public_html/icons/favicon.ico', '../public_html/favicon.ico');
     }
 });
