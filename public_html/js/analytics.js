abe033726585e0259df1220e57cfe4a142a1abc2
Juga Paazmaya <olavic@gmail.com>
2014-10-25 23:17:31 +0300

    Analytics pass linting

--- public_html/js/analytics.js
+++ public_html/js/analytics.js
@@ -8,18 +8,17 @@
 
 'use strict';
 
-(function (T, a, n, t, o, u) {
-  T.GoogleAnalyticsObject = t,
-  T[t] || (T[t] = function () {
+(function analytics(T, a, n, t, o, u) {
+  T.GoogleAnalyticsObject = t;
+  T[t] || (T[t] = function inliner() {
     (T[t].q = T[t].q || []).push(arguments);
-  }),
-  T[t].l = +new Date,
-  o = a.createElement(n),
-  u = a.getElementsByTagName(n)[0],
-  o.src = '//www.google-analytics.com/analytics.js',
+  });
+  T[t].l = +new Date();
+  o = a.createElement(n);
+  u = a.getElementsByTagName(n)[0];
+  o.src = '//www.google-analytics.com/analytics.js';
   u.parentNode.insertBefore(o, u);
-}(this, document, 'script', 'ga'));
-
+})(this, document, 'script', 'ga');
 
 ga('create', 'UA-2643697-14', 'naginata.fi');
 ga('require', 'displayfeatures');
