79cc96e737342ea47e031b9186ccd1ea381c9de7
Juga Paazmaya <paazmaya@yahoo.com>
2017-05-30 20:30:35 +0300

    Building and cleaning

--- source/favicon-creator.js
+++ source/favicon-creator.js
@@ -8,26 +8,26 @@
 
 'use strict';
 
-var fs = require('fs');
-var childProcess = require('child_process');
+const fs = require('fs');
+const childProcess = require('child_process');
 
 /**
  * https://github.com/haydenbleasel/favicons
  * npm install favicons
  */
-var favicons = require('favicons');
+const favicons = require('favicons');
 
 /**
  * https://github.com/donpark/html2jade
  * npm install html2jade
  */
-var html2jade = require('html2jade');
+const html2jade = require('html2jade');
 
-var startTime = (new Date()).getTime();
+const startTime = (new Date()).getTime();
 
-var minifyPng = function () {
+const minifyPng = function () {
   // Minify
-  var child = childProcess.execFile(
+  const child = childProcess.execFile(
     'optipng',
     ['-o7', '../public_html/icons/*.png'],
     {cwd: '.'},
@@ -39,44 +39,43 @@ var minifyPng = function () {
 };
 
 // Convert HTML to Jade
-var convertJade = function () {
-  var fsOpts = {encoding: 'utf8'};
-  var html = fs.readFileSync('favicons.html', fsOpts);
+const convertJade = function () {
+  const html = fs.readFileSync('favicons.html', fsOpts);
   html2jade.convertHtml(html, {}, function (err, jade) {
-    fs.writeFileSync('favicons.jade', jade, fsOpts);
+    fs.writeFileSync('favicons.jade', jade, 'utf8');
   });
 };
 
 favicons({
-    // I/O
-    source: 'naginata-icon.svg',
-    dest: '../public_html/icons',
+  // I/O
+  source: 'naginata-icon.svg',
+  dest: '../public_html/icons',
 
-    // Icon Types
-    android: true,
-    apple: true,
-    coast: true,
-    favicons: true,
-    firefox: true,
-    windows: true,
+  // Icon Types
+  android: true,
+  apple: true,
+  coast: true,
+  favicons: true,
+  firefox: true,
+  windows: true,
 
-    // Miscellaneous
-    html: 'favicons.html',
-    background: '#072434',
-    tileBlackWhite: false,
-    manifest: false,
-    trueColor: true,
-    logging: true,
-    callback: function () {
+  // Miscellaneous
+  html: 'favicons.html',
+  background: '#072434',
+  tileBlackWhite: false,
+  manifest: false,
+  trueColor: true,
+  logging: true,
+  callback: function () {
 
-      minifyPng();
+    minifyPng();
 
-      // Move ico file
-      fs.renameSync('../public_html/icons/favicon.ico', '../public_html/favicon.ico');
+    // Move ico file
+    fs.renameSync('../public_html/icons/favicon.ico', '../public_html/favicon.ico');
 
-      convertJade();
+    convertJade();
 
-      var seconds = Math.round(((new Date()).getTime() - startTime) / 100) / 10;
-      console.log('Completed in ' + seconds + ' seconds. Thanks!');
-    }
+    const seconds = Math.round(((new Date()).getTime() - startTime) / 100) / 10;
+    console.log('Completed in ' + seconds + ' seconds. Thanks!');
+  }
 });
